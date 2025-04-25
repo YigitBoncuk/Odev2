db44445893a79b600ab539ff1ca952a84d1948d6
YigitBoncuk <boncukyigit5@gmail.com>
2025-04-25 17:55:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlindersleri/degiskenler/Odev1.kt
@@ -0,0 +1,107 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+    val sehir = "Bursa"
+    println("Şehir : $sehir")
+    var ulke = "Turkiye"
+    println("Ulke : $ulke")
+    val telefon = 5555555555
+    println(telefon)
+    val postaKodu = 35700
+    println(postaKodu)
+    var mail = "boncukyigit5@gmail.com"
+    println(mail)
+    val meslek = "developer"
+    println(meslek)
+    val stokMiktarı = 100
+    println(stokMiktarı)
+    val musteriAdi = "Yigit"
+    println(musteriAdi)
+    var bakiye = 1589
+    println(bakiye)
+    val dogumGunu = "16 01 2004"
+    println(dogumGunu)
+    var maas = 22002
+    println(maas)
+    var medeniDurum = "bekar"
+    println(medeniDurum)
+    var urunYorum = "Harika!"
+    println(urunYorum)
+    val odemeTarihi = "29 04 2025"
+    println("Odeme Tarihi : $odemeTarihi")
+    var odeme = 15893
+    println(odeme)
+    val siparisAdeti = 3800
+    println("Siparis Adeti : $siparisAdeti")
+    val arabaModeli : String = "Linea"
+    println("Arabamın modeli : $arabaModeli")
+    val kitapAdı = "Abasıyanık"
+    println(kitapAdı)
+    val yayinlanmaTarihi = " ** **** 1565"
+    println(yayinlanmaTarihi)
+    val indirimMiktari = -150
+    println(indirimMiktari)
+    val odaSayisi = "3+1"
+    println(odaSayisi)
+    val enlem = "36-42"
+    println("TR enlem : $enlem")
+    val boylam = "26-45"
+    println("TR boylam : $boylam")
+    val urunAdi = "Sandalye"
+    println("Urun Adi : $urunAdi")
+    var yemekFiyati = 249
+    println("Bu yemek $yemekFiyati$ 'dir")
+    val marka = "nike"
+    println(marka)
+    val muzikAdi = "pop"
+    println(muzikAdi)
+    val videoSuresi = "4 saat"
+    println(videoSuresi)
+    var urunPuani = 4.6
+    println("Urun Puani : $urunPuani")
+    val resimAdi = "Son Aksam Yemegi"
+    println("Resim Adi : $resimAdi")
+    val dosyaFormaati = "pdf"
+    println("Dosya Formati : $dosyaFormaati")
+    var renk = "mavi"
+    println(renk)
+    val renkKodu = 36985
+    println("Mavi renginin kodu : $renkKodu")
+    val telefonModeli = "Iphone"
+    println(telefonModeli)
+    val ekranBoyutu = 16.8
+    println(ekranBoyutu)
+    val agirlik = 25
+    println(agirlik)
+    val ulusalGun = "23 Nisan"
+    println(ulusalGun)
+    val tatilGunu = "1 Mart"
+    println(tatilGunu)
+    var rezervasyonTarihi = "25 Haziran"
+    println(rezervasyonTarihi)
+    val sokakAdi : String = "Tekce"
+    println("Sokak Adi : $sokakAdi")
+    val otobusHatti = "6F"
+    println("bu hat $otobusHatti hattina ait.")
+    var kalanDakika = 6
+    println("Macin bitmesine kalan dakika $kalanDakika dır.")
+    val takipKodu = 1589642
+    println("Takip Kodunuz: $takipKodu dur")
+    var kuponSuresi : String = "29 Gun"
+    println(kuponSuresi)
+    val kuponKodu = 59584162
+    println("Kupon Kodunuz: $kuponKodu dur")
+    val faturaAdresi = "fevzipasa mah ahmet sk no89"
+    println("Fatura Adresi : $faturaAdresi")
+
+
+
+
+
+
+
+
+
+
+
+}
